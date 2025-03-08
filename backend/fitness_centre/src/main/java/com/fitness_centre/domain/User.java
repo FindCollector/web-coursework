34393f2bfbe4ba517accef6ca3b0c08c3b5702d5
huyanjie <huyanjie@MacBook-Pro-2.local>
2025-03-08 16:32:52 +0000

    create User domain

---
+++ backend/fitness_centre/src/main/java/com/fitness_centre/domain/User.java
@@ -0,0 +1,43 @@
+package com.fitness_centre.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author
+ * @Classname User
+ * @Description DONE
+ * @date 08/03/2025
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("fitness_centre")
+public class User implements Serializable {
+    private static final long serialVersionUID = -40356785423868312L;
+
+    @TableId
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private Integer gender;
+
+    private Date birthday;
+
+    private String address;
+
+    private String register_time;
+
+    private Integer status;
+}
